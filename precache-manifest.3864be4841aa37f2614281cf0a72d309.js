4e2902796ca82da18600c7f2dcf447f76acaf97e
BohdanRadchenko <radchenkobs@gmail.com>
2020-02-18 13:56:13 +0200

    Updates

---
+++ precache-manifest.3864be4841aa37f2614281cf0a72d309.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a5678f04b973b19d4c2146cf828042",
+    "url": "/markeboverseas/index.html"
+  },
+  {
+    "revision": "f846f0c79c9d8a05b4c2",
+    "url": "/markeboverseas/static/css/main.410517c3.chunk.css"
+  },
+  {
+    "revision": "61b6f9be3f16e4653640",
+    "url": "/markeboverseas/static/js/2.a79e96bf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/markeboverseas/static/js/2.a79e96bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f846f0c79c9d8a05b4c2",
+    "url": "/markeboverseas/static/js/main.0adc4e34.chunk.js"
+  },
+  {
+    "revision": "3fbf5cfe6d42fee7643b",
+    "url": "/markeboverseas/static/js/runtime-main.6c4d1803.js"
+  },
+  {
+    "revision": "53a99a127dc8764b4ac9289e2e78cb00",
+    "url": "/markeboverseas/static/media/Georgia-Bold.53a99a12.eot"
+  },
+  {
+    "revision": "ce66539e131728b902a6671cdb49c0a4",
+    "url": "/markeboverseas/static/media/Georgia-Bold.ce66539e.woff"
+  },
+  {
+    "revision": "457a113b1fcd5789bba7930ddc946f0a",
+    "url": "/markeboverseas/static/media/Georgia.457a113b.woff"
+  },
+  {
+    "revision": "b905fa45301999c2b63e16b638cc45c9",
+    "url": "/markeboverseas/static/media/Georgia.b905fa45.eot"
+  },
+  {
+    "revision": "e4e98a50fd9c4cdffd271728be32cd88",
+    "url": "/markeboverseas/static/media/header-bg.e4e98a50.jpg"
+  },
+  {
+    "revision": "f6cb19a3887511e5b57b3cf358656ec3",
+    "url": "/markeboverseas/static/media/imgonline-com-ua-Transparent-backgr-koWNgmQvujzU12.f6cb19a3.png"
+  },
+  {
+    "revision": "4320b2baed57993d0307a19eb54faa88",
+    "url": "/markeboverseas/static/media/newsletter-bg.4320b2ba.png"
+  }
+]);
