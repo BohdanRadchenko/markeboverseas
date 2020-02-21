4e5dc5996b83d853a1020dee3b7a74238bdbf489
BohdanRadchenko <radchenkobs@gmail.com>
2020-02-21 13:18:10 +0200

    build forlder

---
+++ build/precache-manifest.23113c259c1cce9d8d7f4b85f14b8c5f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b9e38820f2f1b11ea4c0154b4c9554",
+    "url": "/markeboverseas/index.html"
+  },
+  {
+    "revision": "c01626acdb6e74004a03",
+    "url": "/markeboverseas/static/css/main.6661e43a.chunk.css"
+  },
+  {
+    "revision": "bfdd9e3ebd1743cfd468",
+    "url": "/markeboverseas/static/js/2.08f5a98d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/markeboverseas/static/js/2.08f5a98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01626acdb6e74004a03",
+    "url": "/markeboverseas/static/js/main.f4163143.chunk.js"
+  },
+  {
+    "revision": "3fbf5cfe6d42fee7643b",
+    "url": "/markeboverseas/static/js/runtime-main.6c4d1803.js"
+  },
+  {
+    "revision": "53a99a127dc8764b4ac9289e2e78cb00",
+    "url": "/markeboverseas/static/media/Georgia-Bold.53a99a12.eot"
+  },
+  {
+    "revision": "ce66539e131728b902a6671cdb49c0a4",
+    "url": "/markeboverseas/static/media/Georgia-Bold.ce66539e.woff"
+  },
+  {
+    "revision": "457a113b1fcd5789bba7930ddc946f0a",
+    "url": "/markeboverseas/static/media/Georgia.457a113b.woff"
+  },
+  {
+    "revision": "b905fa45301999c2b63e16b638cc45c9",
+    "url": "/markeboverseas/static/media/Georgia.b905fa45.eot"
+  },
+  {
+    "revision": "bfc9b983b4028011805677d7854507e9",
+    "url": "/markeboverseas/static/media/bg.bfc9b983.png"
+  },
+  {
+    "revision": "e4e98a50fd9c4cdffd271728be32cd88",
+    "url": "/markeboverseas/static/media/header-bg.e4e98a50.jpg"
+  },
+  {
+    "revision": "f6cb19a3887511e5b57b3cf358656ec3",
+    "url": "/markeboverseas/static/media/imgonline-com-ua-Transparent-backgr-koWNgmQvujzU12.f6cb19a3.png"
+  },
+  {
+    "revision": "2aed5bd99a77289025ca5e380be9d569",
+    "url": "/markeboverseas/static/media/licenses_2019 - 2020-1.2aed5bd9.jpg"
+  },
+  {
+    "revision": "ac372b239466e248f9b977b72f1790c7",
+    "url": "/markeboverseas/static/media/licenses_2019 - 2020-2.ac372b23.jpg"
+  },
+  {
+    "revision": "4320b2baed57993d0307a19eb54faa88",
+    "url": "/markeboverseas/static/media/newsletter-bg.4320b2ba.png"
+  }
+]);
